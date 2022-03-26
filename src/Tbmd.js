6f783b78763ab7655f56c60bae21102f15b3cbaa
Moabe <moabeluis2018@gmail.com>
2022-03-26 09:35:19 -0300

    Tmdb

---
+++ src/Tbmd.js
@@ -0,0 +1,52 @@
+const API_KEY = '38c007f28d5b66f36b9c3cf8d8452a4b'
+const API_BASE = 'https://api.themoviedb.org/3'
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`)
+    const json = await req.json()
+    return json
+}
+
+
+export default {
+    getHomeList: async () => {
+        return[
+            {
+                slug: 'orinals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213`)     
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendamos para Você',
+                items: []     
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: []     
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: []     
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: []     
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: []     
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: []     
+            },
+
+        ]
+    }
+}
