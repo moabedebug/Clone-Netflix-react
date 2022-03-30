d2e463ee113ae79b0478a8986a3dcbb6ecffdb32
Moabe <moabeluis2018@gmail.com>
2022-03-29 21:48:01 -0300

    Estilizando Header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+
+export default () => {
+    return(
+        <header>
+            <div className='header--logo'>
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Netflix-new-icon.png/600px-Netflix-new-icon.png" alt="Netflix"/>
+                </a>
+            </div>
+            <div className='header--user'>
+                <a href='/'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7th08ZadbJeY7J3X_2edDTdDpbzbilGuBgzJkKG9yBfpF7OW4zs9hqV9b0rvRK9EeHkk&usqp=CAU" alt="Usuário" />
+                </a>
+            </div>
+        </header>
+    )
+}
